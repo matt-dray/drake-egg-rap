91e9fe5926b334d8ec00576a58d911c39e33ad82
Matt Dray <mwdray@gmail.com>
2019-08-27 19:48:51 +0100

    Provide more information in make.R

--- make.R
+++ make.R
@@ -1,12 +1,13 @@
 # make.R
 
-# Walkthrough: https://ropenscilabs.github.io/drake-manual/intro.html
+# Walkthrough: https://ropenscilabs.github.io/drake-manual/walkthrough.html
+# Download a project template (as used in this demo) with drake_example("main")
+
+
+# Make --------------------------------------------------------------------
+
 
 # Load your packages and supporting functions into your session.
-# If you use supporting scripts like the ones below,
-# you will need to supply them yourself. Examples:
-# https://github.com/wlandau/drake-examples/tree/master/main/R
-
 source("R/packages.R")  # Load your packages, e.g. library(drake).
 source("R/functions.R") # Define your custom code as a bunch of functions.
 source("R/plan.R")      # Create your drake plan.
@@ -14,17 +15,25 @@ source("R/plan.R")      # Create your drake plan.
 # Call make() to run your work.
 # Your targets will be stored in a hidden .drake/ cache,
 # and you can read them back into memory with loadd() and readd().
-
 make(egg_plan)
 
-# Make a config file of your plan
 
-egg_config <- drake_config(egg_plan)
+# Visualise ---------------------------------------------------------------
 
-# Pass the config to a function for generating an interactive dependency graph 
 
-dependency_graph <- vis_drake_graph(egg_config)
+# Optionally, you can visualise the dependency network interactively
+
+# Make a config file of your plan (this creates egg_config$graph for plotting)
+# egg_config <- drake_config(egg_plan)
+
+# Use the config file to generate a interactive dependency graph 
+# dependency_graph <- vis_drake_graph(egg_config)
+
+
+# Make changes ------------------------------------------------------------
+
 
 # Continue to work on your files, then source() them again
 # outdated(egg_config) will print out-of-date files and impacted dependencies
 # Recreate the dependency graph to see this visually
+# Run make(egg_plan) again to bring the workflow up to date
